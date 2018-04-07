fb92bba462ac706da076691f881db7e0837e70c4
Luis Gomez <whoislewys@gmail.com>
2018-04-07 08:57:58 -0700

    Ignore cancelled kickstarters

--- kick-help-model/model/scraper.py
+++ kick-help-model/model/scraper.py
@@ -5,35 +5,45 @@ import requests
 import json
 from lxml import html
 from datetime import datetime
+#import keras
+#from keras.preprocessing.text import text_to_word_sequence
 
 # data_folder = os.path.join(os.getcwd(), '')
 data_folder = os.path.join(os.pardir, 'data')
 dataset1_path = os.path.join(data_folder, 'ks-projects-201612.csv')
 dataset2_path = os.path.join(data_folder, 'ks-projects-201801.csv')
 
+
 def scrape_from_csv(dataset_path):
     # open data
+    X = []
+    Y = []
     with open(dataset_path, 'r') as csvfile:
         dataset1 = csv.reader(csvfile)
         # iterate through projects
         for counter, row in enumerate(dataset1):
-            # ignore header
-            if counter == 0:
+            text_label = row[9]  # possible values: success, failed, canceled (sic)
+            if counter == 0 or text_label == 'canceled': # ignore csv categories and canceled projects
                 continue
-            elif counter == 20:
+            elif counter == 10:
                 break
-            # search for project name
             name = row[1]
             name_pattern = re.compile(name)
             search = requests.get('http://www.kickstarter.com/projects/search.json?search=&term={}'.format(name))
             search_response = search.json()
-            # iterate through results
             for project in search_response['projects']:
-                # if projects match
-                if name_pattern.match(project['name']):
+                if name_pattern.match(project['name']): # search for project name that matches dataset name
                     # scrape
                     project_url = project['urls']['web']['project']
-                    scrape_from_url(project_url)
+                    scrape_results = scrape_from_url(project_url)
+                    X.append(scrape_results)
+
+                    num_label = label_to_number(text_label)
+                    Y.append(num_label)
+    return X, Y
+
+
+
 
 
 def scrape_from_url(project_url):
@@ -79,6 +89,7 @@ def scrape_from_url(project_url):
         pass
     try:
         data['description'] = ''.join(tree.xpath('//div[@class="full-description js-full-description responsive-media formatted-lists"]//p/text()'))
+        # text_to_word_sequence(data['description'])
     except:
         pass
     try:
@@ -92,8 +103,21 @@ def scrape_from_url(project_url):
     # clean
     for key in data:
         data[key] = data[key].replace('\n', ' ').strip()
-    # return
-    return(data)
+
+    # words per line
+
+    return data
+
+
+def label_to_number(text_label): # text_label is success=0, failed=1, canceled=2 (sic)
+    if text_label == 'successful':
+        num_label = 0
+    elif text_label == 'failed':
+        num_label = 1
+    elif text_label == 'canceled':
+        num_label = 2
+    return num_label
+
 
 def get_duration(time1, time2):
     # convert string to datetime
@@ -104,9 +128,10 @@ def get_duration(time1, time2):
     # return
     return(str(t3.total_seconds()))
 
-if __name__ == '__main__':
-    scrape_from_csv(dataset_path=dataset1_path)
 
+if __name__ == '__main__':
+    X, Y = scrape_from_csv(dataset_path=dataset1_path)
+    print("Data: {}\n Label: {}".format(X, Y))
 
 '''
 how to pretty print json:
