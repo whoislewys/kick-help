9f9d93ad28ca53ae9d52fc1d2be34103deedee2b
unknown <ikdiscipulo@gmail.com>
2018-04-07 05:32:20 -0700

    updated scraper

--- kick-help-model/model/scraper.py
+++ kick-help-model/model/scraper.py
@@ -3,6 +3,8 @@ import csv
 import re
 import requests
 import json
+from lxml import html
+from datetime import datetime
 
 #data_folder = os.path.join(os.getcwd(), '')
 data_folder = os.path.join(os.pardir, 'data')
@@ -16,32 +18,55 @@ def scrape_from_csv(dataset_path):
         for counter, row in enumerate(dataset1):
             if counter == 0:
                 continue
+            elif counter == 4:
+                break
             name = row[1]
             name_pattern = re.compile(name)
             search = requests.get('http://www.kickstarter.com/projects/search.json?search=&term={}'.format(name))
             search_response = search.json()
             for project in search_response['projects']:
                 if name_pattern.match(project['name']):
-                    # navigate into project and get description, blurb, etc.
                     project_url = project['urls']['web']['project']
                     scrape_from_url(project_url)
-                    print(project_url)
 
 
 def scrape_from_url(project_url):
-    '''
-    #############
-    YOU GON' NEED INSTALL REQUESTS BOI
-    pip install requests
-    ##########
-    :return:
-    '''
-    # blurb is here div class="NS_project_profiles__blurb"
-    # description is here: div id="full-description"
-    r = requests.get(project_url)
-    # get text of html
-    r = project_url.text
+    # init dictionary
+    data = {'category': '',
+            'blurb': '',
+            'title': '',
+            'duration': '',
+            'goal': '',
+            'description': '',
+            'risks': '',
+            'biography': '',
+            'name': ''}
 
+    # get html tree
+    page = requests.get(project_url)
+    tree = html.fromstring(page.content)
+
+    # get content
+    data['category'] = tree.xpath('//a[@class="nowrap navy-700 flex items-center medium mr3 type-12"]/text()')[0]
+    data['title'] = tree.xpath('//div[@class="col-20-24 col-lg-15-24 hide block-md order-2-md"]//h2/text()')[0]
+    data['blurb'] = tree.xpath('//div[@class="col-20-24 col-lg-15-24 hide block-md order-2-md"]//p/text()')[0]
+    
+    
+    time1 = tree.xpath('//div[@class="NS_campaigns__funding_period"]//p//time[1]/@datetime')[0]
+    time2 = tree.xpath('//div[@class="NS_campaigns__funding_period"]//p//time[2]/@datetime')[0]
+    data['duration'] = get_duration(time1, time2)
+
+    # clean
+    for key in data:
+        data[key] = data[key].replace('\n', ' ').strip()
+        print(data[key])
+
+def get_duration(time1, time2):
+    # year, month, day, hour=0, minute=0, second=0, microsecond=0,
+    t1 = datetime.strptime(time1[0:18], '%Y-%m-%dT%H:%M:%S')
+    t2 = datetime.strptime(time2[0:18], '%Y-%m-%dT%H:%M:%S')
+    t3 = t2 - t1
+    return(str(t3.total_seconds()))
 
 if __name__ == '__main__':
     scrape_from_csv(dataset_path=dataset1_path)
