5f56d2505d105b75eddbd055cb961702f1c963d5
unknown <ikdiscipulo@gmail.com>
2018-04-07 15:40:35 -0700

    update

--- kick_help_api/run_server.py
+++ kick_help_api/run_server.py
@@ -1,7 +1,6 @@
 from flask import request
 import flask
 import scrape
-from kick_help_model.model import word_model as wm
 import pandas as pd
 
 app = flask.Flask(__name__)
@@ -12,20 +11,14 @@ word_model = None
 def load_model():
 	# get global models
 	global model
-	global word_model
-	# define model
-	word_model = wm.load_word_model()
 
 
 @app.route('/predict', methods = ['GET', 'POST'])
 def predict():
 	# get global models
 	global model
-	global word_model
 	# predict
 	url = request.data
-	data = wm.get_word_values('hello', word_model)
-	print(data)
 	return flask.jsonify('test string')
 
 
