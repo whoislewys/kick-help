24d813a270a3371326cb7a841ea6abd9d68a13c2
Sam Rondinelli <sam.rondinelli3@gmail.com>
2018-04-07 13:54:14 -0700

    added url form and page layout

--- kick-help-client/src/pages/Home.js
+++ kick-help-client/src/pages/Home.js
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      url: ''
+    }
+
+    this.handleURLChange = this.handleURLChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+    this.validURL = this.validURL.bind(this);
+  }
+
+  handleURLChange(e) {
+     this.setState({url: e.target.value});
+  }
+
+  validURL() {
+    return true;
+  }
+
+  submitForm() {
+    if(this.validURL()) {
+
+    }
+  }
+
   render() {
     return (
       <div className="form-parent">
         <form>
-          <input type="url" name="name" className="question" id="nme" required autoComplete="off" />
+          <input type="url" name="name" className="question" id="nme" value={this.state.url} onChange={this.handleURLChange} required autoComplete="off" />
           <label htmlFor="url"><span>Project URL</span></label>
-          <input type="submit" value="Submit!" />
+          <input type="button" value="Submit!" onClick={this.submitForm} />
         </form>
       </div>
     );
