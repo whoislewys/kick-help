edf5162487735eb05dcfd30b5ffe71ca7d885e69
unknown <ikdiscipulo@gmail.com>
2018-04-07 12:44:23 -0700

    isaiah update

--- kick-help-model/model/model.py
+++ kick-help-model/model/model.py
@@ -19,8 +19,6 @@ from keras.layers.embeddings import Embedding
 data_folder = os.path.join(os.pardir, 'data')
 dataset1_path = os.path.join(data_folder, 'ks-projects-201612.csv')
 dataset2_path = os.path.join(data_folder, 'ks-projects-201801.csv')
-word_embeddings_path = os.path.join(data_folder, 'glove.6B', 'glove.6B.50d.txt')
-print(word_embeddings_path)
 
 
 def scrape_from_csv(dataset_path):
@@ -132,66 +130,6 @@ def get_duration(time1, time2):
     t3 = t2 - t1
     # return
     return(str(t3.total_seconds()))
-
-
-def load_word2vec_embeddings():
-    # https://machinelearningmastery.com/develop-word-embeddings-python-gensim/
-    # http://www.orbifold.net/default/2017/01/10/embedding-and-tokenizer-in-keras/
-    embeddings_index = {}
-    glove_data = word_embeddings_path
-    f = open(glove_data, encoding='utf-8')
-    for line in f:
-        values = line.split()
-        word = values[0]
-        value = np.asarray(values[1:], dtype='float32')
-        embeddings_index[word] = value
-    f.close()
-    print('Loaded %s word vectors.' % len(embeddings_index))
-    return embeddings_index
-
-
-def preprocess(embeddings_index, train_x, train_y):
-
-    '''
-    This function will preprocess kickstarter text
-    '''
-
-    # The embedding_matrix matrix maps words to vectors in the specified embedding dimension (here 10):
-    embedding_dimension = 10
-    tokenizer = Tokenizer() # allows you to prepare text in various ways for neural nets
-
-    texts = ["The sun is shining in June!", "September is grey.", "Life is beautiful in August.", "I like it", "This and other things?"]
-    tokenizer.fit_on_texts(texts)
-    word_index = tokenizer.word_index
-
-    embedding_matrix = np.zeros((len(word_index) + 1, embedding_dimension))
-    for word, i in word_index.items():
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:
-            # words not found in embedding index will be all-zeros.
-            embedding_matrix[i] = embedding_vector[:embedding_dimension]
-
-    #Now you have an embedding matrix of 19 words into dimension 10:
-    print('Embedding matrix shape: ', embedding_matrix.shape)
-
-    # define a new keras Embedding layer
-    embedding_layer = Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1], weights=[embedding_matrix], input_length=12)
-
-
-    # TODO: resolve missing word_index
-
-    X = tokenizer.texts_to_sequences(texts)
-    X = pad_sequences(X, maxlen=19)
-    y = [1,0,0,0,0]
-    vocab_size = len(tokenizer.word_index) + 1
-
-    model = Sequential()
-    model.add(Dense(2, input_dim=vocab_size))
-
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='rmsprop')
-    # eventually use x_train instead of placeholder X
-    model.fit(X, y=train_y, batch_size=200, epochs=700, verbose=0, validation_split=0.2, shuffle=True)
 
 
 def train(embeddings_index, train_x, train_y):
