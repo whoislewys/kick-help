5471958ee88c0c2c55a56aa335059986690ef065
unknown <ikdiscipulo@gmail.com>
2018-04-07 03:17:37 -0700

    created server

--- kick-help-api/run-keras-server.py
+++ kick-help-api/run-keras-server.py
@@ -1,2 +1,24 @@
-from keras.applications import ResNet50
-from keras.preprocessing.image import img_to_array
+import numpy as np
+import flask
+import io
+
+app = flask.Flask(__name__)
+model = None
+
+def load_model():
+	global model
+	# define model
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+	data = {'success': False}
+	if flask.request.method == 'POST':
+		data['success'] = True
+
+	return flask.jsonify(data)
+
+if __name__ == '__main__':
+	print(('* loading keras model and flask starting server...'
+		'please wait until server has fully started'))
+	load_model()
+	app.run()
